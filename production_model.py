0a3a471e2db99052b40a7f718d2d9ae7eee9f5ea
Andreas Svensson <andreas.svensson@iths.se>
2023-03-29 14:12:32 +0200

    printing class report and saving cm plot to file

--- production_model.py
+++ production_model.py
@@ -1,11 +1,28 @@
 import joblib
 import pandas as pd
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
 
 # read in model and the 100 datapoint test samples
 model = joblib.load("models/final_model.pkl")
 df = pd.read_csv("assets/test_samples.csv")
 
-probabilities = model.predict_proba(df.drop("cardio", axis = 1))
+# X and y from the 100 datapoints
+X_test = df.drop("cardio", axis = 1)
+y_test = df["cardio"]
+
+# predict probabilities and class
+probabilities = model.predict_proba(X_test) # probabilities
+y_pred = model.predict(X_test) # classification
+
+# print class report
+print(classification_report(y_test, y_pred))
+
+# save results cm as .png
+cm = confusion_matrix(y_test, y_pred)
+fig = ConfusionMatrixDisplay(cm, display_labels = ["No", "Yes"]).plot()
+plt.suptitle("Production Model Results")
+plt.savefig("assets/production_model_results.png")
 
 # create a dataframe of predicted probabilities
 df_probabilities = pd.DataFrame(probabilities, columns  = ["probability class 0", "probability class 1"])
